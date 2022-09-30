7a5384517cfe9e1cd3061d2a0d252ae4c341b663
Felix Bade <flix.bade@gmail.com>
2022-10-01 00:17:43 +0300

    Send latest article link to Telegram

--- scraper.py
+++ scraper.py
@@ -1,11 +1,32 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
+from dotenv import dotenv_values
+
+from telegram import getMe, sendMessage
+
+config = {
+    **dotenv_values('.env'),
+    **os.environ,
+}
+
+mandatory_variables = [
+    'TELEGRAM_TOKEN',
+    'TELEGRAM_CHAT'
+]
+
+for mandatory_variable in mandatory_variables:
+    if not mandatory_variable in config:
+        print(f'{mandatory_variable} needs to be set as an environment variable or in .env')
+        exit(1)
 
 url = 'https://www.hs.fi/paakirjoitukset/'
 
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text, 'html5lib')
 
+article_links = []
 articles = soup.find_all('article')
 for article in articles:
     if article.a:
@@ -15,4 +36,12 @@ for article in articles:
             continue
         
         link = f'https://www.hs.fi{link}'
+        article_links.append(link)
         print(link)
+
+tg_token = config['TELEGRAM_TOKEN']
+tg_chat = config['TELEGRAM_CHAT']
+print()
+print(getMe(tg_token))
+print()
+print(sendMessage(tg_token, tg_chat, article_links[0]))
---
+++ telegram.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+# https://core.telegram.org/bots/api
+# 'https://api.telegram.org/bot<token>/METHOD_NAME'
+
+def getMe(token):
+    resp = requests.get(f'https://api.telegram.org/bot{token}/getMe')
+    return json.loads(resp.text)
+
+def sendMessage(token, chat_id, text):
+    resp = requests.post(f'https://api.telegram.org/bot{token}/sendMessage', data={'chat_id': chat_id, 'text': text})
+    return json.loads(resp.text)
